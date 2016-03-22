8d036c965eb1c39e8fd120e651b30edfa9065691
Daniel Thor Kristjansson <danielk@cuymedia.net>
2016-03-22 16:54:45 -0400

    Make output function smarter.
    This enables keys to only be present for some of our time periods and
    it allow us to keep private data in the dictionary (prefxed by an underscore)

--- test/unit/test_output_statistics.py
+++ test/unit/test_output_statistics.py
@@ -11,18 +11,23 @@ class OutputStatisticsTests(TestCase):
 
     def setUp(self):
         """Pre-test setup"""
-        pass
+        stats = {
+            timestring.Date("1/1/2016 1:00:00"): {"s1": 121212},
+            timestring.Date("1/1/2016 1:01:00"): {"s2": 232323, "_fun": "baddata"}}
+        out = StringIO.StringIO()
+        output_csv(out, stats)
+        self.outstr = out.getvalue()
+        out.close()
 
     def test_output_csv_no_stats_no_exception(self):
         output_csv(None, {})
 
-    def test_output_csv_outputs_data(self):
-        stats = {
-            timestring.Date("1/1/2016 1:00:00"): {"s": 121212},
-            timestring.Date("1/1/2016 1:01:00"): {"s": 232323}}
-        out = StringIO.StringIO()
-        out.write("write something\n")
-        output_csv(out, stats)
-        assert "121212" in out.getvalue()
-        assert "232323" in out.getvalue()
-        out.close()
+    def test_output_csv_outputs_keys_in_order(self):
+        assert "when,s1,s2" in self.outstr
+
+    def test_output_csv_outputs_values_in_place(self):
+        assert ",121212," in self.outstr
+        assert ",,232323" in self.outstr
+
+    def test_output_csv_ignores_underscore_data(self):
+        assert "baddata" not in self.outstr
--- toofast/output_statistics.py
+++ toofast/output_statistics.py
@@ -3,15 +3,37 @@ import csv
 
 
 def output_csv(output_file, stats):
-    '''Writes statistics to an output CSV file'''
+    '''
+    Writes statistics to an output CSV file
+
+    This assumes stats is a dictonary of the form:
+    {
+       "when 1": { "key1": "value1",  "key2": "value2" }
+       "when 2": { "key1": "value1",  "key3": "value3" }
+    }
+
+    All the keys plus the when column are extracted to become the header of the CSV.
+    Then the values from each sub dictionary in the stats data are extracted one to a row.
+    You can also be assured that the keys will be sorted into a stable ordering.
+
+    Finally, you may store private data in the internal dictionaries so long as the
+    key it is stored under is prefixed the an underscore "_".
+
+    In this example:
+
+    when,key1,key2,key3
+    when 1,value1,value2,
+    when 2,value1,,value3
+    '''
     rows = sorted(stats.keys())
     if not rows:
         return
 
     writer = csv.writer(output_file)
-    stats_keys = sorted(stats[rows[0]].keys())
+    stats_key_set = set([key for row in rows for key in stats[row].keys() if key[0:1] != "_"])
+    stats_keys = sorted(list(stats_key_set))
     writer.writerow(["when"] + stats_keys)
 
     for row in rows:
-        values = [stats[row][key] for key in stats_keys]
+        values = [stats[row].get(key, "") for key in stats_keys]
         writer.writerow([str(row)] + values)
